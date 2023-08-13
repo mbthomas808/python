14d30e9a0f328c9737fc407d18f71a207ad5282e
mbthomas808 <130110418+mbthomas808@users.noreply.github.com>
2023-08-13 16:46:31 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,11 +1,16 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Initial function to be run on start-up
+        Name Param: The name of the account
+        """
         self.__account_name = name
         self.__account_balance = 0.00
 
-    def deposit(self, amount) -> bool:
+    def deposit(self, amount: float) -> bool:
         """
         Add money
+        Amount param: The amount looking to be deposited
         """
         try:
             amount = float(amount)
@@ -17,9 +22,10 @@ class Account:
             self.__account_balance += amount
             return True
 
-    def withdrawal(self, amount) -> bool:
+    def withdrawal(self, amount: float) -> bool:
         """
         Subtract Money
+        Amount param: The amount looking to be withdrawn
         """
         try:
             amount = float(amount)
@@ -34,11 +40,13 @@ class Account:
     def get_balance(self) -> float:
         """
         Retreive balance of account
+        Returns account balance, a private variable
         """
         return self.__account_balance
 
     def get_name(self) -> str:
         """
         Retreive name on account
+        Returns account name, a private variable
         """
         return self.__account_name
